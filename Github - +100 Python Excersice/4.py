a23cb6a8073a184d6d30f44e9e1bdbcf7d9d394d
Ramin Shahrivar <89016694+rahriver@users.noreply.github.com>
2022-04-06 17:22:58 +0430

    Delete Github - +100 Python Excersice directory

--- Github - +100 Python Excersice/4.py
+++
@@ -1,6 +0,0 @@
-# .split()
-
-numbers = input("Numbers comma sep: ").split(',')
-print(tuple(numbers))
-print(numbers)
-
--- Github - +100 Python Excersice/7.py
+++
@@ -1,8 +0,0 @@
-x = int(input("X: "))
-y = int(input("Y: "))
-l = []
-i = 0
-while i < x:
-    l.append([j*i for j in range(0,y)])
-    i+=1
-print(l)
